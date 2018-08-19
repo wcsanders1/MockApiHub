d16fe018cf592b84119f40a50f9e4a3bf77e54a5
William Sanders <william.sanders.dev@gmail.com>
2018-08-19 14:25:25 -0400

    Add route pkg with untested func to add route to tree

---
+++ route/route.go
@@ -0,0 +1,77 @@
+package route
+
+import (
+	"fmt"
+	"strings"
+	"errors"
+)
+
+type (
+	routeType int
+	
+	// Tree contains routing for an API in a tree format
+	Tree struct {
+		routeType routeType
+		branches map[string]*Tree
+	}
+)
+
+const (
+	incomplete routeType = 0
+	complete   routeType = 1
+	wildCard   routeType = 2
+)
+
+// NewRouteTree returns a new instance of RouteTree
+func NewRouteTree() *Tree {
+	return &Tree {
+		routeType: incomplete,
+		branches: make(map[string]*Tree),
+	}
+}
+
+// AddRoute adds a route to the tree
+func (tree *Tree) AddRoute(route string) error {
+	fragments := strings.Split(route, "/")
+	if err := tree.addRoute(fragments); err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	return nil
+}
+
+func (tree *Tree) addRoute(fragments []string) error {
+	if len(fragments) == 0 {
+		return nil
+	}
+
+	curFrag := fragments[0]
+	remFrags := fragments[1:]
+
+	if branch, exists := tree.branches[curFrag]; exists {
+		return branch.addRouteToExistingBranch(remFrags)
+	}
+	
+	tree.branches[curFrag] = NewRouteTree()
+	if len(remFrags) > 0 {
+		return tree.branches[curFrag].addRouteToExistingBranch(remFrags)
+	}
+
+	tree.branches[curFrag].routeType = complete
+
+	return nil
+}
+
+func (tree *Tree) addRouteToExistingBranch(remFrags []string) error {
+	if len(remFrags) == 0 {
+		if (tree.routeType == complete) {
+			return errors.New("route already exists")
+		}
+		tree.routeType = complete
+		
+		return nil
+	}
+
+	return tree.addRoute(remFrags)
+}
