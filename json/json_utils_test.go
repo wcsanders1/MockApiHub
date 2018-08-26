d5da0a9cbade3f3411812b6424b5e4c7aa8bc740
William Sanders <william.sanders.dev@gmail.com>
2018-08-26 14:16:27 -0400

    Test isValidJSON

---
+++ json/json_utils_test.go
@@ -0,0 +1,30 @@
+package json
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIsValidJSON(t *testing.T) {
+	goodJSON := []byte(`{
+							"JSON": "good",
+							"test": "good"
+					   }`)
+	badJSON :=  []byte(`{
+						    "JSON": "bad,
+						    "test": "good"
+						}`)
+	
+	result := isValidJSON(goodJSON)
+
+	assert := assert.New(t)
+	assert.True(result)
+
+	result = isValidJSON(badJSON)
+
+	assert.False(result)
+
+	result = isValidJSON([]byte(""))
+
+	assert.False(result)
+}
