0266a9d5fffed1edf76f0e28d05b7a3774b099c0
William Sanders <william.sanders.dev@gmail.com>
2018-10-04 06:43:44 -0400

    Test CleanURL and untested conditions in str_utils

--- str/str_utils_test.go
+++ str/str_utils_test.go
@@ -23,6 +23,28 @@ func TestGetURLFragments(t *testing.T) {
 	assert.Equal(2, len(frags))
 	assert.Equal("test", frags[0])
 	assert.Equal("url", frags[1])
+
+	emptyURL := ""
+	frags, err = GetURLFragments(emptyURL)
+
+	assert.Nil(frags)
+	assert.NotNil(err)
+	assert.Error(err)
+}
+
+func TestCleanURL(t *testing.T) {
+	url := "/TESt/Url/"
+	result := CleanURL(url)
+
+	assert := assert.New(t)
+	assert.NotNil(result)
+	assert.NotEmpty(result)
+	assert.Equal("test/url", result)
+
+	emptyURL := ""
+	result = CleanURL(emptyURL)
+
+	assert.Empty(result)
 }
 
 func TestRemoveColonFromParam(t *testing.T) {
@@ -31,13 +53,18 @@ func TestRemoveColonFromParam(t *testing.T) {
 
 	assert := assert.New(t)
 	assert.Equal("id", niceParam)
+
+	emptyStr := ""
+	assert.Empty(RemoveColonFromParam(emptyStr))
 }
 
 func TestIsParam(t *testing.T) {
 	param := ":id"
 	nonParam := "id"
+	emptyStr := ""
 
 	assert := assert.New(t)
 	assert.True(IsParam(param))
 	assert.False(IsParam(nonParam))
+	assert.False(IsParam(emptyStr))
 }
