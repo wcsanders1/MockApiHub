8706d5f24ea11fdfb7f98ceed0b9872643f105ed
William Sanders <william.sanders.dev@gmail.com>
2018-10-08 07:22:48 -0400

    Begin to test GetJSON

--- json/json_utils_test.go
+++ json/json_utils_test.go
@@ -1,21 +1,37 @@
 package json
 
 import (
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestIsValidJSON(t *testing.T) {
-	goodJSON := []byte(`{
-							"JSON": "good",
-							"test": "good"
-					   }`)
-	badJSON := []byte(`{
-						    "JSON": "bad,
-						    "test": "good"
-						}`)
+type fakeBasicOps struct{}
 
+var goodJSON = []byte(`{
+	"JSON": "good",
+	"test": "good"
+}`)
+var badJSON = []byte(`{
+	"JSON": "bad,
+	"test": "good"
+}`)
+
+func (ops *fakeBasicOps) Open(file string) (*os.File, error) {
+	return os.NewFile(1, "fakefile"), nil
+}
+
+func (ops *fakeBasicOps) ReadAll(file *os.File) ([]byte, error) {
+	return goodJSON, nil
+}
+
+// TODO: Finish this
+func TestGetJSON(t *testing.T) {
+
+}
+
+func TestIsValidJSON(t *testing.T) {
 	result := isValidJSON(goodJSON)
 
 	assert := assert.New(t)
