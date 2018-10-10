9a1405c87d1f70184d3e71cd977b2e2179d0e7be
William Sanders <william.sanders.dev@gmail.com>
2018-10-10 07:00:15 -0400

    Test fail conditions in GetJSON

--- json/json_utils_test.go
+++ json/json_utils_test.go
@@ -1,6 +1,7 @@
 package json
 
 import (
+	"errors"
 	"os"
 	"testing"
 
@@ -33,16 +34,47 @@ func (ops *fakeBasicOps) Open(file string) (*os.File, error) {
 
 func TestGetJSON(t *testing.T) {
 	filePath := "testpath"
-	basicOps := new(fakeBasicOps)
-	basicOps.On("Open", mock.AnythingOfType("string")).Return(os.NewFile(1, "fakefile"), nil)
-	basicOps.On("ReadAll", mock.AnythingOfType("*os.File")).Return(goodJSON, nil)
+	basicOpsPass := new(fakeBasicOps)
+	basicOpsPass.On("Open", mock.AnythingOfType("string")).Return(os.NewFile(1, "fakefile"), nil)
+	basicOpsPass.On("ReadAll", mock.AnythingOfType("*os.File")).Return(goodJSON, nil)
 
-	result, err := GetJSON(filePath, basicOps)
+	result, err := GetJSON(filePath, basicOpsPass)
 
 	assert := assert.New(t)
 	assert.NotNil(result)
 	assert.Nil(err)
-	basicOps.AssertCalled(t, "Open", filePath)
+	basicOpsPass.AssertCalled(t, "Open", filePath)
+	basicOpsPass.AssertCalled(t, "ReadAll", mock.AnythingOfType("*os.File"))
+
+	basicOpsOpenFail := new(fakeBasicOps)
+	basicOpsOpenFail.On("Open", mock.AnythingOfType("string")).Return(os.NewFile(1, "fakefile"), errors.New(""))
+
+	result2, err2 := GetJSON(filePath, basicOpsOpenFail)
+
+	assert.NotNil(err2)
+	assert.Nil(result2)
+	basicOpsOpenFail.AssertCalled(t, "Open", filePath)
+	basicOpsOpenFail.AssertNotCalled(t, "ReadAll", mock.AnythingOfType("*os.File"))
+
+	basicOpsReadAllFail := new(fakeBasicOps)
+	basicOpsReadAllFail.On("Open", mock.AnythingOfType("string")).Return(os.NewFile(1, "fakefile"), nil)
+	basicOpsReadAllFail.On("ReadAll", mock.AnythingOfType("*os.File")).Return(goodJSON, errors.New(""))
+
+	result3, err3 := GetJSON(filePath, basicOpsReadAllFail)
+	assert.NotNil(err3)
+	assert.Nil(result3)
+	basicOpsReadAllFail.AssertCalled(t, "Open", filePath)
+	basicOpsReadAllFail.AssertCalled(t, "ReadAll", mock.AnythingOfType("*os.File"))
+
+	basicOpsBadJSON := new(fakeBasicOps)
+	basicOpsBadJSON.On("Open", mock.AnythingOfType("string")).Return(os.NewFile(1, "fakefile"), nil)
+	basicOpsBadJSON.On("ReadAll", mock.AnythingOfType("*os.File")).Return(badJSON, nil)
+
+	result4, err4 := GetJSON(filePath, basicOpsBadJSON)
+	assert.NotNil(err4)
+	assert.Nil(result4)
+	basicOpsBadJSON.AssertCalled(t, "Open", filePath)
+	basicOpsBadJSON.AssertCalled(t, "ReadAll", mock.AnythingOfType("*os.File"))
 }
 
 func TestIsValidJSON(t *testing.T) {
