0eeac8a7df1900d03f4e6eb4215d450bb518da58
William Sanders <william.sanders.dev@gmail.com>
2018-10-12 07:18:29 -0400

    Test decodeAPIConfig

--- manager/manager_test.go
+++ manager/manager_test.go
@@ -1,12 +1,18 @@
 package manager
 
 import (
+	"errors"
+	"fmt"
 	"testing"
+
+	"github.com/BurntSushi/toml"
 
 	"github.com/wcsanders1/MockApiHub/api"
 	"github.com/wcsanders1/MockApiHub/config"
+	"github.com/wcsanders1/MockApiHub/fake"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
 func TestNewManager(t *testing.T) {
@@ -91,4 +97,39 @@ func TestApiByPortExists(t *testing.T) {
 
 	result = mgr.apiByPortExists(port + 1)
 	assert.False(result)
+}
+
+func TestDecodeAPIConfig(t *testing.T) {
+	basicOpsPass := new(fake.BasicOps)
+	basicOpsPass.On("DecodeFile", mock.AnythingOfType("string"), mock.Anything).Return(toml.MetaData{}, nil)
+
+	mgrPass := Manager{
+		file: basicOpsPass,
+	}
+
+	dir := "testDir"
+	apiDir := "./api/apis"
+	fileName := "testfile"
+	path := fmt.Sprintf("%s/%s/%s", apiDir, dir, fileName)
+
+	result, err := mgrPass.decodeAPIConfig(dir, fileName)
+
+	assert := assert.New(t)
+	assert.Nil(err)
+	assert.NotNil(result)
+	assert.IsType(&config.APIConfig{}, result)
+	basicOpsPass.AssertCalled(t, "DecodeFile", path, mock.AnythingOfType("*config.APIConfig"))
+
+	basicOpsFail := new(fake.BasicOps)
+	basicOpsFail.On("DecodeFile", mock.AnythingOfType("string"), mock.Anything).Return(toml.MetaData{}, errors.New(""))
+
+	mgrFail := Manager{
+		file: basicOpsFail,
+	}
+
+	result2, err2 := mgrFail.decodeAPIConfig(dir, fileName)
+	assert.Error(err2)
+	assert.Nil(result2)
+	assert.IsType(&config.APIConfig{}, result2)
+	basicOpsFail.AssertCalled(t, "DecodeFile", path, mock.AnythingOfType("*config.APIConfig"))
 }
