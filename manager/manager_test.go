91d777f43f6669b378cc5f4cece48a7d2b44aea0
William Sanders <william.sanders.dev@gmail.com>
2018-10-14 17:20:16 -0400

    Test apiByPortExists

--- manager/manager_test.go
+++ manager/manager_test.go
@@ -3,6 +3,7 @@ package manager
 import (
 	"testing"
 
+	"github.com/wcsanders1/MockApiHub/api"
 	"github.com/wcsanders1/MockApiHub/config"
 
 	"github.com/stretchr/testify/assert"
@@ -32,4 +33,48 @@ func TestNewManager(t *testing.T) {
 
 	assert.Nil(result)
 	assert.Error(err)
+}
+
+func TestApiByPortExists(t *testing.T) {
+	testPort := 4000
+
+	mgr := Manager{
+		apis: make(map[string]*api.API),
+	}
+
+	firstAPIConfig := config.APIConfig{
+		HTTP: config.HTTP{
+			Port: 3999,
+		},
+	}
+	secondAPIConfig := config.APIConfig{
+		HTTP: config.HTTP{
+			Port: 4001,
+		},
+	}
+
+	firstAPI, err1 := api.NewAPI(&firstAPIConfig)
+	secondAPI, err2 := api.NewAPI(&secondAPIConfig)
+
+	mgr.apis["firstAPI"] = firstAPI
+	mgr.apis["secondAPI"] = secondAPI
+
+	result := mgr.apiByPortExists(testPort)
+	assert := assert.New(t)
+	assert.False(result)
+	assert.Nil(err1)
+	assert.Nil(err2)
+
+	thirdAPIConfig := config.APIConfig{
+		HTTP: config.HTTP{
+			Port: testPort,
+		},
+	}
+
+	thirdAPI, err3 := api.NewAPI(&thirdAPIConfig)
+	mgr.apis["thirdAPI"] = thirdAPI
+
+	result2 := mgr.apiByPortExists(testPort)
+	assert.True(result2)
+	assert.Nil(err3)
 }
