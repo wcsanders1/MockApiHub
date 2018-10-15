d529db5861f23a79288d6899b62ab4fcc0a8f59e
William Sanders <william.sanders.dev@gmail.com>
2018-10-15 07:21:33 -0400

    Add fake api

---
+++ api/apis/api_fake.go
@@ -0,0 +1,48 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/stretchr/testify/mock"
+	"github.com/wcsanders1/MockApiHub/config"
+)
+
+// FakeAPI is a mockable API
+type FakeAPI struct {
+	mock.Mock
+}
+
+// Register is a mockable api.Register()
+func (api *FakeAPI) Register(dir, defaultCert, defaultKey string) error {
+	args := api.Called(dir, defaultCert, defaultKey)
+	return args.Error(0)
+}
+
+// Shutdown is a mockable api.Shutdown()
+func (api *FakeAPI) Shutdown() error {
+	args := api.Called()
+	return args.Error(0)
+}
+
+// ServeHTTP is a mockable api.ServeHTTP()
+func (api *FakeAPI) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	return
+}
+
+// GetPort is a mockable api.GetPort()
+func (api *FakeAPI) GetPort() int {
+	args := api.Called()
+	return args.Int(0)
+}
+
+// GetBaseURL is a mockable api.GetBaseURL()
+func (api *FakeAPI) GetBaseURL() string {
+	args := api.Called()
+	return args.String(0)
+}
+
+// GetEndpoints is a mockable api.GetEndpoints()
+func (api *FakeAPI) GetEndpoints() map[string]config.Endpoint {
+	args := api.Called()
+	return args.Get(0).(map[string]config.Endpoint)
+}
