3de5827c66385d0f542221f343bcf9b0255dd92d
William Sanders <william.sanders.dev@gmail.com>
2018-10-20 12:03:37 -0400

    Add serverops wrapper

---
+++ wrapper/serverops_wrapper.go
@@ -0,0 +1,40 @@
+package wrapper
+
+import (
+	"context"
+	"net/http"
+)
+
+type (
+	// IServerOps contains basic server operations
+	IServerOps interface {
+		Shutdown(context.Context) error
+		ListenAndServe() error
+		ListenAndServeTLS(string, string) error
+	}
+
+	// ServerOps provides a real implementation of IServerOps
+	ServerOps struct {
+		server *http.Server
+	}
+)
+
+// NewServerOps returns a pointer to a new ServerOps
+func NewServerOps(server *http.Server) *ServerOps {
+	return &ServerOps{server}
+}
+
+// Shutdown shuts down the server
+func (ops *ServerOps) Shutdown(ctx context.Context) error {
+	return ops.server.Shutdown(ctx)
+}
+
+// ListenAndServe starts the server
+func (ops *ServerOps) ListenAndServe() error {
+	return ops.server.ListenAndServe()
+}
+
+// ListenAndServeTLS starts the server using TLS
+func (ops *ServerOps) ListenAndServeTLS(certFile, keyFile string) error {
+	return ops.server.ListenAndServeTLS(certFile, keyFile)
+}
