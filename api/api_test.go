5d7218a8ef91b53964ab56a54b835d2c7952a21b
William Sanders <william.sanders.dev@gmail.com>
2018-10-29 07:17:12 -0400

    Test ensureRouteRegistered

--- api/api_test.go
+++ api/api_test.go
@@ -5,9 +5,46 @@ import (
 	"testing"
 
 	"github.com/wcsanders1/MockApiHub/config"
+	"github.com/wcsanders1/MockApiHub/route"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
+
+func TestEnsureRouteRegistered_AddsRoute_IfNotRegistered(t *testing.T) {
+	url := "test/url"
+	fakeRouteTree := &route.FakeTree{}
+	fakeRouteTree.On("GetRoute", mock.AnythingOfType("string")).Return("", map[string]string{}, nil)
+	fakeRouteTree.On("AddRoute", mock.AnythingOfType("string")).Return(url, nil)
+	api := &API{
+		routeTree: fakeRouteTree,
+	}
+
+	result := api.ensureRouteRegistered(url)
+
+	assert := assert.New(t)
+	assert.NotEmpty(result)
+	assert.Equal(url, result)
+	fakeRouteTree.AssertCalled(t, "GetRoute", url)
+	fakeRouteTree.AssertCalled(t, "AddRoute", url)
+}
+
+func TestEnsureRouteRegistered_DoesNotAddRoute_IfRegistered(t *testing.T) {
+	url := "test/url"
+	fakeRouteTree := &route.FakeTree{}
+	fakeRouteTree.On("GetRoute", mock.AnythingOfType("string")).Return(url, map[string]string{}, nil)
+	api := &API{
+		routeTree: fakeRouteTree,
+	}
+
+	result := api.ensureRouteRegistered(url)
+
+	assert := assert.New(t)
+	assert.NotEmpty(result)
+	assert.Equal(url, result)
+	fakeRouteTree.AssertCalled(t, "GetRoute", url)
+	fakeRouteTree.AssertNotCalled(t, "AddRoute", mock.Anything)
+}
 
 func TestCreateAPIServer_ReturnsServer_WhenPortProvided(t *testing.T) {
 	httpConfig := &config.HTTP{
