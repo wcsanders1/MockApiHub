1dda06a4b1d0f9edf61259a94179032841061759
William Sanders <william.sanders.dev@gmail.com>
2018-11-01 07:19:05 -0400

    Begin to test Start in api pkg

---
+++ api/api_creator_fake.go
@@ -0,0 +1,24 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/wcsanders1/MockApiHub/config"
+	"github.com/wcsanders1/MockApiHub/wrapper"
+
+	"github.com/stretchr/testify/mock"
+)
+
+type fakeAPICreator struct {
+	mock.Mock
+}
+
+func (c *fakeAPICreator) getHandler(enforceValidJSON bool, dir, fileName string, file wrapper.IFileOps) func(w http.ResponseWriter, r *http.Request) {
+	args := c.Called(enforceValidJSON, dir, fileName, file)
+	return args.Get(0).(func(w http.ResponseWriter, r *http.Request))
+}
+
+func (c *fakeAPICreator) startAPI(defaultCert, defaultKey string, server wrapper.IServerOps, httpConfig config.HTTP) error {
+	args := c.Called(defaultCert, defaultKey, server, httpConfig)
+	return args.Error(0)
+}
